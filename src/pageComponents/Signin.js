94f4f4fadf18f42d9202f53431b7ffeed89cbd92
NeilanshTriesToCode <neilanshjr@gmail.com>
2021-06-09 18:27:45 +0530

    added auth to sign-in user, working on adding user data to database (incomplete)

--- src/pageComponents/Signin.js
+++ src/pageComponents/Signin.js
@@ -1,8 +1,82 @@
 // React Component to sign-in user
 import React from 'react';
+import firebase from 'firebase/app';
+import { Alert, Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite';
+
+import '../styles/utility.scss';
+import { auth, database } from '../misc/firebase';
 
 const Signin = () => {
-  return <div>Sign-in</div>;
+    // provider is either Facebook or Google
+    const signInWithProvider = async provider => {
+        try {
+            /* auth.signInWithPopup opens up a pop-up window for the clicked sign-in option, 
+                    and asks to enter credentials to sign the user in. */
+            // using auth function to extract signed in user info
+            // after authorizing user, it gets stored under "Authorization" on firebase website
+            // a user ID gets created when a user is authorized
+            const { additionalUserInfo, user } = await auth.signInWithPopup(
+                provider
+            );
+
+            // if the user is newUser
+            if (additionalUserInfo.isNewUser) {
+                // add user to firebase database
+                // the ref() function takes as an argument the path under which the data would be stored
+                database.ref(`/profiles/${user.uid}`);
+            }
+
+            Alert.success('Signed in', 4000); // using Alert from rsuite
+        } catch (err) {
+            Alert.info(err.message, 4000); // using Alert from rsuite
+        }
+    };
+
+    // sign-in with FaceBook
+    const onFacebookSignIn = () => {
+        signInWithProvider(new firebase.auth.FacebookAuthProvider()); // new firebase.auth.FacebookAuthProvider() returns a provider object
+    };
+
+    // sign-in with Google
+    const onGoogleSignIn = () => {
+        signInWithProvider(new firebase.auth.GoogleAuthProvider()); // new firebase.auth.GoogleAuthProvider() returns a provider object
+    };
+
+    return (
+        <Container>
+            <Grid className="mt-page">
+                <Row>
+                    <Col xs={24} md={12} mdOffset={6}>
+                        <Panel>
+                            <div className="text-center">
+                                <h2>Welcome to chat</h2>
+                                <p>Progressive chat platform for neophytes</p>
+                            </div>
+
+                            <div className="mt-3">
+                                <Button
+                                    block
+                                    color="blue"
+                                    onClick={onFacebookSignIn}
+                                >
+                                    <Icon icon="facebook" /> Continue with
+                                    Facebook
+                                </Button>
+
+                                <Button
+                                    block
+                                    color="red"
+                                    onClick={onGoogleSignIn}
+                                >
+                                    <Icon icon="google" /> Continue with Google
+                                </Button>
+                            </div>
+                        </Panel>
+                    </Col>
+                </Row>
+            </Grid>
+        </Container>
+    );
 };
 
 export default Signin;
