d3ffd6965ebc3632ef92e344e1858398d2a02254
NeilanshTriesToCode <neilanshjr@gmail.com>
2021-06-09 21:44:31 +0530

    added user data to firebase database

--- src/pageComponents/Signin.js
+++ src/pageComponents/Signin.js
@@ -14,16 +14,21 @@ const Signin = () => {
                     and asks to enter credentials to sign the user in. */
             // using auth function to extract signed in user info
             // after authorizing user, it gets stored under "Authorization" on firebase website
-            // a user ID gets created when a user is authorized
+            // a user UID gets created when a user is authorized
             const { additionalUserInfo, user } = await auth.signInWithPopup(
                 provider
             );
 
-            // if the user is newUser
+            // if the signed-in user is newUser (newUser == true)
             if (additionalUserInfo.isNewUser) {
                 // add user to firebase database
                 // the ref() function takes as an argument the path under which the data would be stored
-                database.ref(`/profiles/${user.uid}`);
+                // REMINDER: the database is JSON-based
+                // set() function is used to store the data in database in JSON format. It returns a promise
+                database.ref(`/profiles/${user.uid}`).set({
+                    name: user.displayName,
+                    createdAt: firebase.database.ServerValue.TIMESTAMP,
+                });
             }
 
             Alert.success('Signed in', 4000); // using Alert from rsuite
