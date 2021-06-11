012101d8e5a7ddf268ed875f5fe1ae00c57a314a
NeilanshTriesToCode <neilanshjr@gmail.com>
2021-06-11 15:53:58 +0530

    started building dashboard, displayed user name and Sign out button

--- src/components/Dashboard/DashboardToggle.js
+++ src/components/Dashboard/DashboardToggle.js
@@ -1,13 +1,24 @@
 // React Component for Dashboard Toggle
-import React from 'react';
-import { Button, Drawer, Icon } from 'rsuite';
+import React, { useCallback } from 'react';
+import { Alert, Button, Drawer, Icon } from 'rsuite';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
 import Dashboard from '.';
+import { auth } from '../../misc/firebase';
 
 const DashboardToggle = () => {
     const { isOpen, open, close } = useModalState();
 
     const isMobile = useMediaQuery('(max-width: 992px)'); // returns true or false
+
+    // function to Sign-out user (to be sent as a prop to the Dashboard Component)
+    const onSignOut = useCallback(() => {
+        // sign user out
+        auth.signOut();
+
+        Alert.info('Signed out successfully', 4000);
+
+        close(); // update the state of the Drawer
+    }, [close]);
 
     return (
         <>
@@ -20,7 +31,7 @@ const DashboardToggle = () => {
                 onHide={close}
                 placement="left"
             >
-                <Dashboard />
+                <Dashboard onSignOut={onSignOut} />
             </Drawer>
         </>
     );
--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,8 +1,34 @@
 // React Component to contain code for Dashboard Component
+// the Dashboard Component is written inside the DashboardToggle Component's Drawer Component
 import React from 'react';
+import { Drawer, Button } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 
-const Dashboard = () => {
-    return <div>hello</div>;
+const Dashboard = ({ onSignOut }) => {
+    // getting access to Profile Context
+    /* 
+        Since ProfileContextProvider wraps the Homepage Component, it also
+        encompasses its children Components.
+    */
+    const { profile } = useProfile();
+
+    return (
+        <>
+            <Drawer.Header>
+                <Drawer.Title>Dashboard</Drawer.Title>
+            </Drawer.Header>
+
+            <Drawer.Body>
+                <h3>Hey, {profile.name} </h3>
+            </Drawer.Body>
+
+            <Drawer.Footer>
+                <Button block color="red" onClick={onSignOut}>
+                    Sign out
+                </Button>
+            </Drawer.Footer>
+        </>
+    );
 };
 
 export default Dashboard;
