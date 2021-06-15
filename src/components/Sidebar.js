35658c9418c528e9414af01c64f912a87a1a77f4
NeilanshTriesToCode <neilanshjr@gmail.com>
2021-06-15 15:53:11 +0530

    added Create Chat room functionality

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import DashboardToggle from './Dashboard/DashboardToggle';
 import '../styles/utility.scss';
+import CreateRoomBtnModal from './Dashboard/CreateRoomBtnModal';
 
 const Sidebar = () => {
     return (
         <div className="h-100 pt-2">
             <div>
                 <DashboardToggle />
+                <CreateRoomBtnModal />
             </div>
             bottom
         </div>
