badc8a122f1ba40d1d631c018f27fc7b557aeb69
NeilanshTriesToCode <neilanshjr@gmail.com>
2021-06-15 18:18:14 +0530

    created Rooms Context

--- src/misc/helpers.js
+++ src/misc/helpers.js
@@ -12,3 +12,33 @@ export function getNameInitials(name) {
     // else return initial of the firstname
     return splitName[0][0];
 }
+
+// function to convert database snapshot values from JSON to array
+export function transformToArrayWithId(snapVal) {
+    /* snapVal refers to the contents of snap.val() used inside the callback function inside the on() function,
+        while retrieving the contents of the database. It has the contents of the database in JSON format,
+        which is to be converted into an array.
+    */
+    /* if snapVal is not null, return an array of objects where each object consists of a key and its values 
+        from snapVal
+        eg: suppose snapVal = {
+                        roomId: {
+                            name,
+                            description
+                        }
+                    } 
+            then return in this form: [
+                                        {
+                                            name,
+                                            description,
+                                            roomId
+                                        }
+                                    ]
+           
+    */
+    return snapVal
+        ? Object.keys(snapVal).map(roomId => {
+              return { ...snapVal[roomId], roomId };
+          })
+        : [];
+}
--- src/pageComponents/Homepage.js
+++ src/pageComponents/Homepage.js
@@ -2,17 +2,20 @@
 import React from 'react';
 import { Grid, Row, Col } from 'rsuite';
 import Sidebar from '../components/Sidebar';
+import { RoomsProvider } from '../context/rooms.context';
 import '../styles/utility.scss';
 
 const Homepage = () => {
     return (
-        <Grid fluid className="h-100">
-            <Row className="h-100">
-                <Col xs={24} md={8} className="h-100">
-                    <Sidebar />
-                </Col>
-            </Row>
-        </Grid>
+        <RoomsProvider>
+            <Grid fluid className="h-100">
+                <Row className="h-100">
+                    <Col xs={24} md={8} className="h-100">
+                        <Sidebar />
+                    </Col>
+                </Row>
+            </Grid>
+        </RoomsProvider>
     );
 };
 
